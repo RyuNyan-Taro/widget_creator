7669b1a616049e40b6e598bb4d3b8b5f67b08901
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-02-15 21:34:18 +0900

    add name text.

--- lib/view/has_sub_widget_view.dart
+++ lib/view/has_sub_widget_view.dart
@@ -24,15 +24,38 @@ class _CharacterCard extends StatelessWidget {
       height: 300,
       child: Card(
         color: Colors.red,
-        child: const Padding(
-            padding: EdgeInsets.all(16.0),
-            child: Text(
-              'This is a super red Card!',
-              style: TextStyle(color: Colors.white),
-            )
-        ),
+        child: Column(
+          children: [
+            const _NameAndType(name: 'Taro', type: 'Normal'),
+            const _NameAndType(name: 'Hanako', type: 'Super'),
+          ],
+        )
+      )
+    );
+  }
+
+}
+
+class _NameAndType extends StatelessWidget {
+  const _NameAndType ({super.key, required this.name, required this.type});
+
+  final String name;
+  final String type;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Text(name),
+          Spacer(flex: 1), // Spacerでスペースを確保
+          Text(type),
+          Spacer(flex: 2), // Spacerでスペースを確保
+        ],
       ),
     );
   }
+
 
 }
