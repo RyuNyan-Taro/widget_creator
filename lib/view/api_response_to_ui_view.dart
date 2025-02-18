3f3fb7235872ce89cb1abe965856c60ad6a98e19
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-02-18 22:30:14 +0900

    doing create api_response_to_ui_view.dart and switch page to it.

---
+++ lib/view/api_response_to_ui_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ApiResponseToUiPage extends StatelessWidget {
+  const ApiResponseToUiPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Api Response To Ui Page'),
+      ),
+      body: const Center(
+        child: Text('Hello, this is Api Response To Ui Page.'),
+      ),
+    );
+  }
+}
