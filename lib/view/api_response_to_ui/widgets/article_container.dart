0d4b0aa9821036092d512a957c877b79413a515d
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-02-22 20:47:35 +0900

    divide to sub class for readable.

--- lib/view/api_response_to_ui/widgets/article_container.dart
+++ lib/view/api_response_to_ui/widgets/article_container.dart
@@ -9,21 +9,38 @@ class ArticleContainer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
+      padding: _ArticlePadding.padding,
+      child: _ArticleMain(article: article),
+    );
+  }
+}
+
+class _ArticlePadding{
+  const _ArticlePadding();
+
+  static const padding = EdgeInsets.symmetric(
+    vertical: 12,
+    horizontal: 16,
+  );
+}
+
+class _ArticleMain extends StatelessWidget {
+  const _ArticleMain({super.key, required this.article});
+
+  final Article article;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
       padding: const EdgeInsets.symmetric(
-        vertical: 12,
-        horizontal: 16,
+        horizontal: 20,
+        vertical: 16,
       ),
-      child: Container(
-        padding: const EdgeInsets.symmetric(
-          horizontal: 20,
-          vertical: 16,
-        ),
-        height: 180,
-        decoration: const BoxDecoration(
-          color: Color(0xFF55C500), // ← 背景色を指定
-          borderRadius: BorderRadius.all(
-            Radius.circular(32), // ← 角丸を設定
-          ),
+      height: 180,
+      decoration: const BoxDecoration(
+        color: Color(0xFF55C500), // ← 背景色を指定
+        borderRadius: BorderRadius.all(
+          Radius.circular(32), // ← 角丸を設定
         ),
       ),
     );
