6595cdd5370d4d9507e2431491baaf5642a7f980
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-02-22 20:30:47 +0900

    modify to add space between ArticleContainers.

--- lib/view/api_response_to_ui/widgets/article_container.dart
+++ lib/view/api_response_to_ui/widgets/article_container.dart
@@ -1,20 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:widget_creator/view/api_response_to_ui/models/article.dart';
 
 class ArticleContainer extends StatelessWidget {
-  const ArticleContainer({super.key});
+  const ArticleContainer({super.key, required this.article});
+
+  final Article article;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
+    return Padding(
       padding: const EdgeInsets.symmetric(
-        horizontal: 20,
-        vertical: 16,
+        vertical: 12,
+        horizontal: 16,
       ),
-      height: 180,
-      decoration: const BoxDecoration(
-        color: Color(0xFF55C500), // ← 背景色を指定
-        borderRadius: BorderRadius.all(
-          Radius.circular(32), // ← 角丸を設定
+      child: Container(
+        padding: const EdgeInsets.symmetric(
+          horizontal: 20,
+          vertical: 16,
+        ),
+        height: 180,
+        decoration: const BoxDecoration(
+          color: Color(0xFF55C500), // ← 背景色を指定
+          borderRadius: BorderRadius.all(
+            Radius.circular(32), // ← 角丸を設定
+          ),
         ),
       ),
     );
