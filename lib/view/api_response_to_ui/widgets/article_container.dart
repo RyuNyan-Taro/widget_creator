67d90fc5e783b1b273e372dc9cd4f9cc3537e762
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-02-22 21:08:38 +0900

    delete unused key.

--- lib/view/api_response_to_ui/widgets/article_container.dart
+++ lib/view/api_response_to_ui/widgets/article_container.dart
@@ -26,7 +26,7 @@ class _ArticlePadding{
 }
 
 class _ArticleMain extends StatelessWidget {
-  const _ArticleMain({super.key, required this.article});
+  const _ArticleMain({required this.article});
 
   final Article article;
 
