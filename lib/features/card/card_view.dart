00298d53dde61cde05707185b05b63e7e1c092f8
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-04-29 07:47:34 +0900

    divide with parts

--- lib/features/card/card_view.dart
+++ lib/features/card/card_view.dart
@@ -4,6 +4,7 @@ import 'package:widget_creator/features/square_tiles/square_tiles_view.dart';
 import 'package:widget_creator/features/has_sub_widget/has_sub_widget_view.dart';
 import 'package:widget_creator/features/api_response_to_ui/screens/api_response_to_ui_view.dart';
 import 'package:widget_creator/shared/dummy_view.dart';
+part '_my_cards.dart';
 
 
 class CardPage extends StatelessWidget {
@@ -21,94 +22,5 @@ class CardPage extends StatelessWidget {
             child: _MyCards(),
           ),
         ));
-  }
-}
-
-class _MyCards extends StatelessWidget {
-  const _MyCards();
-
-  @override
-  Widget build(BuildContext context) {
-
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: List.generate(_cardDataList.length, (index) {
-        return Padding(
-          padding: const EdgeInsets.only(bottom: 20),
-          child: _TextCard(
-              text: _cardDataList[index].text,
-              nextPage: _cardDataList[index].nextPage
-          )
-        );
-      }),
-    );
-  }
-}
-
-final List<_CardData> _cardDataList = [
-  _CardData(
-    text: 'Horizontal and vertical',
-    nextPage: () => const HorizontalAndVerticalPage(),
-  ),
-  _CardData(
-    text: 'Square tiles',
-    nextPage: () => const SquareTilesPage(),
-  ),
-  _CardData(
-    text: 'Has sub widgets',
-    nextPage: () => const HasSubWidgetPage(),
-  ),
-  _CardData(
-    text: 'Update with state',
-    nextPage: () => const DummyPage(),
-  ),
-  _CardData(
-    text: 'Action buttons',
-    nextPage: () => const DummyPage(),
-  ),
-  _CardData(
-    text: 'API response to UI',
-    nextPage: () => ApiResponseToUiPage(),
-  )
-];
-
-
-class _CardData {
-  final String text;
-  final Widget Function() nextPage;
-
-  _CardData({required this.text, required this.nextPage});
-}
-
-class _TextCard extends StatelessWidget {
-
-  final String text;
-  final Widget Function() nextPage;
-
-  const _TextCard({
-    required this.text,
-    required this.nextPage
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      onTap: () {
-        Navigator.push(context, PageRouteBuilder(
-          pageBuilder: (context, animation, secondaryAnimation) =>
-          nextPage(),
-          transitionsBuilder:
-              (context, animation, secondaryAnimation, child) {
-            return FadeTransition(
-              opacity: animation,
-              child: child,
-            );
-          },
-        ),);
-      },
-      leading: Icon(Icons.person_2_rounded),
-      title : Text(text)
-    );
   }
 }
