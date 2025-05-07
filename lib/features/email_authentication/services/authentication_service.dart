594a07f55d6bff8ff449cc065655929bf239740e
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-05-07 15:18:08 +0900

    add login function

--- lib/features/email_authentication/services/authentication_service.dart
+++ lib/features/email_authentication/services/authentication_service.dart
@@ -20,4 +20,18 @@ class AuthService {
       throw Exception('some thing is wrong in authentication: $error');
     }
   }
+
+  Future<AuthResponse> loginWithPassword(
+      {required String email, required String password}) async {
+    try {
+      return await _supabase.auth.signInWithPassword(
+        email: email,
+        password: password,
+      );
+    } on AuthException catch (error) {
+      throw Exception('authentication error: ${error.message}');
+    } on Exception catch (error) {
+      throw Exception('some thing is wrong in authentication: $error');
+    }
+  }
 }
