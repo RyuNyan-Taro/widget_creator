5a0ef0f1127018a7fd05d5af899ff1546faea54a
RyuNyan-Taro <ryuryuryu1905@gmail.com>
2025-05-10 14:27:04 +0900

    refactor to extract log in form

--- lib/features/email_authentication/screens/email_authentication_screen.dart
+++ lib/features/email_authentication/screens/email_authentication_screen.dart
@@ -6,50 +6,8 @@ import 'package:widget_creator/features/email_authentication/screens/reset_passw
 import 'package:widget_creator/features/email_authentication/screens/signup_screen.dart';
 import 'package:widget_creator/features/email_authentication/services/authentication_service.dart';
 
-class LoginPage extends StatefulWidget {
+class LoginPage extends StatelessWidget {
   const LoginPage({super.key});
-
-  @override
-  State<LoginPage> createState() => _LoginPageState();
-}
-
-class _LoginPageState extends State<LoginPage> {
-  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  bool isLoading = false;
-  final AuthService authClient = AuthService();
-
-  Future<void> _handleLogin() async {
-    if (!_formKey.currentState!.validate()) {
-      return;
-    }
-
-    try {
-      setState(() => isLoading = true);
-
-      final response = await authClient.loginWithPassword(
-        email: _emailController.text,
-        password: _passwordController.text,
-      );
-
-      if (!mounted) return;
-
-      if (response.user == null) return;
-
-      Navigator.of(context).pop();
-    } on AuthException catch (e) {
-      if (!mounted) return;
-      await _showErrorDialog(context, 'Authentication error: ${e.message}');
-    } on Exception catch (e) {
-      if (!mounted) return;
-      await _showErrorDialog(context, 'Unknown error: $e');
-    } finally {
-      if (mounted) {
-        setState(() => isLoading = false);
-      }
-    }
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -59,35 +17,103 @@ class _LoginPageState extends State<LoginPage> {
       ),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
-        child: Form(
-          key: _formKey,
-          autovalidateMode: AutovalidateMode.onUserInteraction,
-          child: Column(
-            children: <Widget>[
-              _ValidateForm(
-                  controller: _emailController,
-                  formLabel: 'Email',
-                  validateText: 'Please enter your email'),
-              _ValidateForm(
-                  controller: _passwordController,
-                  formLabel: 'Password',
-                  validateText: 'Please enter your password'),
-              const SizedBox(height: 12.0),
-              ElevatedButton(
-                child: isLoading
-                    ? const CircularProgressIndicator()
-                    : const Text('Login'),
-                onPressed: () async => await _handleLogin(),
-              ),
-              _LinkText(
-                  text: 'Go to Signup',
-                  builder: (context) => const SignUpPage()),
-              _LinkText(
-                  text: 'Do you forget password?',
-                  builder: (context) => const ResetPasswordPage())
-            ],
-          ),
+        child: Column(
+          children: <Widget>[
+            _LoginForm(
+              onLoginSuccess: (user) {
+                Navigator.of(context).pop();
+              },
+            ),
+            const SizedBox(height: 8.0),
+            _LoginLinks(),
+          ],
         ),
+      ),
+    );
+  }
+}
+
+class _LoginForm extends StatefulWidget {
+  const _LoginForm({
+    required this.onLoginSuccess,
+  });
+
+  final void Function(User user) onLoginSuccess;
+
+  @override
+  State<_LoginForm> createState() => _LoginFormState();
+}
+
+class _LoginFormState extends State<_LoginForm> {
+  final _formKey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  bool _isLoading = false;
+  final _authClient = AuthService();
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  Future<void> _handleLogin() async {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
+    try {
+      setState(() => _isLoading = true);
+
+      final response = await _authClient.loginWithPassword(
+        email: _emailController.text,
+        password: _passwordController.text,
+      );
+
+      if (!mounted) return;
+
+      if (response.user == null) return;
+
+      widget.onLoginSuccess(response.user!);
+    } on AuthException catch (e) {
+      if (!mounted) return;
+      await _showErrorDialog(context, 'Authentication error: ${e.message}');
+    } on Exception catch (e) {
+      if (!mounted) return;
+      await _showErrorDialog(context, 'Unknown error: $e');
+    } finally {
+      if (mounted) {
+        setState(() => _isLoading = false);
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Form(
+      key: _formKey,
+      autovalidateMode: AutovalidateMode.onUserInteraction,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          _ValidateForm(
+            controller: _emailController,
+            formLabel: 'Email',
+            validateText: 'Please enter your email',
+          ),
+          _ValidateForm(
+            controller: _passwordController,
+            formLabel: 'Password',
+            validateText: 'Please enter your password',
+          ),
+          ElevatedButton(
+            onPressed: _isLoading ? null : _handleLogin,
+            child: _isLoading
+                ? const CircularProgressIndicator()
+                : const Text('Login'),
+          ),
+        ],
       ),
     );
   }
@@ -137,9 +163,28 @@ class _ValidateForm extends StatelessWidget {
   }
 }
 
+class _LoginLinks extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch, // 横幅いっぱいに広げる
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        _LinkText(
+          text: 'Go to Signup',
+          builder: (context) => const SignUpPage(),
+        ),
+        _LinkText(
+          text: 'Do you forget password?',
+          builder: (context) => const ResetPasswordPage(),
+        ),
+      ],
+    );
+  }
+}
+
 class _LinkText extends StatelessWidget {
   const _LinkText({
-    super.key,
     required this.text,
     required this.builder,
   });
